b5b7e64e689916abd0c9f361234e31042447d0ed
Joshua Drakeford <JD@Administrators-MacBook-Pro.local>
2024-08-20 14:43:04 -0400

    updated cypress testing

--- cypress/e2e/navbar.cy.js
+++ cypress/e2e/navbar.cy.js
@@ -1,15 +1,25 @@
 describe('Navbar Navigation', () => {
-    it('should navigate to the Quiz Generation page when the link is clicked', () => {
-      cy.visit('http://localhost:3000/');
-      cy.contains('Quiz Generation').click();
-      cy.url().should('include', '/quizgeneration');
-      cy.contains('Quiz Generation Options');
-    });
-    
-    it('should navigate to the Account page when the link is clicked', () => {
-      cy.visit('http://localhost:3000/');
-      cy.contains('Account').click();
-      cy.url().should('include', '/account');
-      cy.contains('You have a streak of 5 days!');
-    });
+  // test for navigating to the Quiz Generation page
+  it('should navigate to the Quiz Generation page when the link is clicked', () => {
+    // visit the homepage
+    cy.visit('http://localhost:3000/');
+    // click on the 'Quiz Generation' link
+    cy.contains('Quiz Generation').click();
+    // verify the URL includes '/quizgeneration'
+    cy.url().should('include', '/quizgeneration');
+    // check for the presence of 'Quiz Generation Options' text
+    cy.contains('Quiz Generation Options');
   });
+  
+  // test for navigating to the Account page
+  it('should navigate to the Account page when the link is clicked', () => {
+    // visit the homepage
+    cy.visit('http://localhost:3000/');
+    // click on the 'Account' link
+    cy.contains('Account').click();
+    // verify the URL includes '/account'
+    cy.url().should('include', '/account');
+    // check for the presence of 'You have a streak of 5 days!' text
+    cy.contains('You have a streak of 5 days!');
+  });
+});
